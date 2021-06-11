18cbb814eb8c9d70617c88ae580f308da78e2a51
Luis Loaeza <luis.loaeza@hotmail.com>
2021-06-11 18:17:53 -0500

    Conneced Database to AWS

--- cryptotracker/settings.py
+++ cryptotracker/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'cryptotracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cryptotracker',
+        'HOST': 'cryptotracker.c1lrgmovngsy.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'USER': 'admin',
+        'PASSWORD': 'Welcome1!',
     }
 }
 
