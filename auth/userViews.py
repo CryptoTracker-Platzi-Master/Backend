ffe49807626f632bffa002bc2c0351c0ddf78def
cexperto <sipuedeshazloya@gmail.com>
2021-06-16 16:11:45 -0500

    import generics

--- auth/userViews.py
+++ auth/userViews.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 from .userSerializer import UserSerializer
 
 
