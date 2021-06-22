84f016c007d041b56ec19efd7af6a79cbb666862
miguelalf <miguel.alfva@hotmail.com>
2021-06-22 10:31:02 -0600

    Hotfix - use env vars for email in settings

--- cryptotracker/settings.py
+++ cryptotracker/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
     ]
 }
 
-EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'postmaster@sandbox004e8673e466446c93251a1ce59e9b7a.mailgun.org'
-EMAIL_HOST_PASSWORD = '520fa9055fafbe0d5b21b06c9f74fe98-1f1bd6a9-35cd31a0'
+EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
+EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
+EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
+EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
