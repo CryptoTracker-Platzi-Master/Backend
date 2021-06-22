2edfddc0f18031048323268bc4b4ab088bc7a093
miguelalf <miguel.alfva@hotmail.com>
2021-06-22 05:11:13 -0600

    Auth - verify token | validate current token

--- auth/urls.py
+++ auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import Login, Signup
+from .views import Login, Signup, Validation
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('token/', views.obtain_auth_token),
     path('login/', Login.as_view()),
     path('signup/', Signup.as_view()),
+    path('validate/', Validation.as_view())
 ]
